5aa8f4d5f110aaca75aa0e8d8d23aac874ef87a7
NoOp <dominik@zodl.com>
2026-03-16 10:00:26 +0100

    MOB-78: Implemented randomized chart line mask

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/authentication/view/WelcomeAnimation.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/authentication/view/WelcomeAnimation.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
@@ -132,11 +131,8 @@ fun WelcomeScreenView(
                                 .height(screenHeight.overallScreenHeight()),
                         contentDescription = null
                     )
-                    Image(
-                        painter = painterResource(id = co.electriccoin.zcash.ui.design.R.drawable.chart_line),
-                        contentScale = ContentScale.FillBounds,
-                        colorFilter = ColorFilter.tint(color = ZcashTheme.colors.welcomeAnimationColor),
-                        contentDescription = null,
+                    DynamicChartLine(
+                        color = ZcashTheme.colors.welcomeAnimationColor,
                     )
                 }
 
